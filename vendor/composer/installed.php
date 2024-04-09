2c1690c1005cfe75d0c3dc46b325e595b94dca58
Azad <AzadPC1324@outlook.com>
2024-04-09 21:56:46 +0330

    .

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lanous/apimaker',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f7074fa6ae6bdd91790791ebd6b8c90aecfec081',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lanous/apimaker' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f7074fa6ae6bdd91790791ebd6b8c90aecfec081',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
